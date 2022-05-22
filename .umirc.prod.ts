bb8ec864af746598a7c46d4b50b89a306ed3e0fb
wjkang <ruoxieme@gmail.com>
2022-05-22 18:30:29 +0800

    update

--- .umirc.prod.ts
+++ .umirc.prod.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'umi';
 const define = {
   ENV: 'prod',
   USE_MOCK: false,
-  API_HOST: 'http://yapi.smart-xwork.cn/mock/129987',
+  API_HOST: 'https://yapi.smart-xwork.cn/mock/129987',
 };
 export default defineConfig({
   define,
